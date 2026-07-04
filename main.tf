8ffec09a03cad30a7907f91cef7de72434a889c0
Inna Boiko <146464243+InnaIvBoiko@users.noreply.github.com>
2026-07-04 16:18:02 +0200

    Increase EKS instance type from t3.small to t3.medium for improved performance and resource allocation

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ module "eks" {
 
   cluster_name  = "${var.project_name}-eks"
   subnet_ids    = module.vpc.private_subnets
-  instance_type = "t3.small"
+  instance_type = "t3.medium" # same 2 vCPU as t3.small, but 4GiB RAM and a higher pod-per-node limit (17 vs 11)
   desired_size  = 3
   max_size      = 4
   min_size      = 1
